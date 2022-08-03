2c58b3648eed25ffa4155e5e07fb82fff66ffa37
Nguyen Quang Tiep <60705570+nguyenquangtiep222@users.noreply.github.com>
2022-08-03 20:24:02 +0700

    1

--- Tools/Update/Update/MainWindow.xaml.cs
+++ Tools/Update/Update/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Net;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Update
 {
@@ -23,6 +14,67 @@ namespace Update
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Closed(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void btnTest_Click(object sender, RoutedEventArgs e)
+        {
+            var directLink = "https://dl.dropboxusercontent.com/s/evb3atcaba7ke7d/Update.7z?dl=0";
+            var localPath = @"C:\Users\nguyenquangtiep\Desktop\Update.7z";
+            //var directLink = "https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv";
+            //var localPath = @"C:\Users\nguyenquangtiep\Desktop\addresses.csv";
+            DownloadFile(directLink, localPath);
+        }
+
+        public void DownloadFile(string directLink, string localPath)
+        {
+            Task.Run(() =>
+            {
+                try
+                {
+                    Console.WriteLine("Downloading...");
+                    using (var client = new WebClient())
+                    {
+                        client.DownloadProgressChanged += Client_DownloadProgressChanged;
+                        client.DownloadFileCompleted += Client_DownloadFileCompleted;
+                        var buffer = client.DownloadData(directLink);
+                        using (var stream = new FileStream(localPath, FileMode.Create))
+                        {
+                            var bw = new BinaryWriter(stream);
+                            bw.Write(buffer);
+
+                            //// Read text
+                            //var bytes = new byte[stream.Length];
+                            //stream.Read(bytes, 0, (int)stream.Length);
+                            //var data = System.Text.Encoding.ASCII.GetString(bytes);
+                        }
+                    }
+                    Console.WriteLine("Download completed!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            });
+        }
+
+        private void Client_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+
+        }
+
+        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+
         }
     }
 }
--- Tools/Update/Update/Properties/AssemblyInfo.cs
+++ Tools/Update/Update/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -10,9 +8,9 @@ using System.Windows;
 [assembly: AssemblyTitle("Update")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Update")]
-[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
