806d348e91e2b4bcdc322c094020ddb02e56706e
Nguyen Quang Tiep <60705570+nguyenquangtiep222@users.noreply.github.com>
2022-09-18 17:40:57 +0700

    Update MainWindow.xaml.cs

--- Tools/Update/Update/MainWindow.xaml.cs
+++ Tools/Update/Update/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
 
 namespace Update
 {
@@ -17,16 +20,44 @@ namespace Update
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             this.Title = "Update";
+            StartupApp();
         }
 
         private void Window_Closed(object sender, System.EventArgs e)
         {
-
+            ShutdownApp();
         }
 
-        private void Unzip()
+        private void StartupApp()
         {
+            try
+            {
+                var processes = Process.GetProcessesByName("Store");
+                if (processes.Length > 0)
+                {
+                    foreach (var process in processes)
+                        process.Kill();
+                }
+                Thread.Sleep(1000);
+                Application.Current.Shutdown();
+                Environment.Exit(0);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
+        private void ShutdownApp()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
