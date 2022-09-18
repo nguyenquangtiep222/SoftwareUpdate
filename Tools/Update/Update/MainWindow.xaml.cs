e6135a437145a54fdb43e7556bbe656f29dcb9bc
Nguyen Quang Tiep <60705570+nguyenquangtiep222@users.noreply.github.com>
2022-09-18 17:31:58 +0700

    1

--- Tools/Update/Update/MainWindow.xaml.cs
+++ Tools/Update/Update/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Net;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace Update
 {
@@ -11,6 +7,8 @@ namespace Update
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Properties.Settings _param = Properties.Settings.Default;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -18,7 +16,7 @@ namespace Update
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
+            this.Title = "Update";
         }
 
         private void Window_Closed(object sender, System.EventArgs e)
@@ -26,53 +24,7 @@ namespace Update
 
         }
 
-        private void btnTest_Click(object sender, RoutedEventArgs e)
-        {
-            var directLink = "https://dl.dropboxusercontent.com/s/evb3atcaba7ke7d/Update.7z?dl=0";
-            var localPath = @"C:\Users\nguyenquangtiep\Desktop\Update.7z";
-            //var directLink = "https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv";
-            //var localPath = @"C:\Users\nguyenquangtiep\Desktop\addresses.csv";
-            DownloadFile(directLink, localPath);
-        }
-
-        public void DownloadFile(string directLink, string localPath)
-        {
-            Task.Run(() =>
-            {
-                try
-                {
-                    Console.WriteLine("Downloading...");
-                    using (var client = new WebClient())
-                    {
-                        client.DownloadProgressChanged += Client_DownloadProgressChanged;
-                        client.DownloadFileCompleted += Client_DownloadFileCompleted;
-                        var buffer = client.DownloadData(directLink);
-                        using (var stream = new FileStream(localPath, FileMode.Create))
-                        {
-                            var bw = new BinaryWriter(stream);
-                            bw.Write(buffer);
-
-                            //// Read text
-                            //var bytes = new byte[stream.Length];
-                            //stream.Read(bytes, 0, (int)stream.Length);
-                            //var data = System.Text.Encoding.ASCII.GetString(bytes);
-                        }
-                    }
-                    Console.WriteLine("Download completed!");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            });
-        }
-
-        private void Client_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
-        {
-
-        }
-
-        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        private void Unzip()
         {
 
         }
--- Tools/Update/Update/Properties/Settings.Designer.cs
+++ Tools/Update/Update/Properties/Settings.Designer.cs
@@ -8,22 +8,42 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Update.Properties
-{
-
-
+namespace Update.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("update.7z")]
+        public string UpdateFile {
+            get {
+                return ((string)(this["UpdateFile"]));
+            }
+            set {
+                this["UpdateFile"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("version.txt")]
+        public string VersionFile {
+            get {
+                return ((string)(this["VersionFile"]));
+            }
+            set {
+                this["VersionFile"] = value;
             }
         }
     }
